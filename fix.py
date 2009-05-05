bee501d0ed32529d1c16a09891f38792e582ea8b
Sander Smits <jhmsmits@gmail.com>
2009-05-05 21:21:05 +0200

    removed _ from _sources and _static, otherwise gh-pages ignores those files

--- fix.py
+++ fix.py
@@ -1,11 +1,21 @@
 import os
+import shutil
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
 
 def main():
+    """not functional at this time -> do it by find and replace
+    
+    find '_sources' replace by 'sources'
+    find '_static' replace by 'static'
+    """
     to_change = ['_sources', '_static']
+    include_file_ext = ['html', 'js']
     for f in os.listdir(ROOT):
-        if os.path.isdir(f) and f in to_change:
+        if os.path.isdir(f):
+            if f in to_change:
+                shutil.move(os.path.join(ROOT, f), os.path.join(ROOT, f[1:]))
+        else:
             print f
 
 if __name__ == '__main__':
